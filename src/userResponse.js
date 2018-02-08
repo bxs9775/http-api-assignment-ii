a08e4859a4dabf344749ad741b62ad4b7ca479ff
BRIAN SANDON (RIT Student) <bxs9775@ad.rit.edu>
2018-02-08 13:36:28 -0500

    Fixed addUsers()

--- src/userResponse.js
+++ src/userResponse.js
@@ -18,16 +18,56 @@ const jsonMessageToXML = (json) => {
   return xmlObj;
 };
 
+const addUserFromBody = (request, response, body) => {
+  const accept = request.headers.accept.split(',');
+  const jsonResponse = {
+    message: 'Both name and age are required in the response.',
+  };
+
+  if (!body) {
+    return response;
+  }
+
+  console.dir(body);
+
+  if (!body.name || !body.age) {
+    jsonResponse.id = 'missingParams';
+    if (accept[0] === 'text/xml') {
+      const xmlObj = jsonMessageToXML(jsonResponse);
+      baseResponse.writeResponse(response, 400, xmlObj, accept[0]);
+    } else {
+      baseResponse.writeResponse(response, 400, JSON.stringify(jsonResponse), 'application/json');
+    }
+  }
+
+  const userObj = {
+    name: body.name,
+    age: body.age,
+  };
+
+  const newUser = !(users[body.name]);
+  users[body.name] = userObj;
+
+  if (!newUser) {
+    baseResponse.writeErrorHead(response, 204, accept);
+  } else {
+    baseResponse.writeError(response, 201, accept);
+  }
+};
+
 const parseBody = (request, response) => {
+  console.log('Running parseBody();');
+
   // Stores parts of the body when they are loaded in
   const body = [];
 
   // Creates an event error to handle things when an error occurs.
   request.on('error', (err) => {
+    console.dir('OnError');
+
     console.dir(err);
     response.statusCode = 400;
     response.end();
-    return null;
   });
 
   // Identifies when data is retrieved and adds it to the body object.
@@ -37,13 +77,15 @@ const parseBody = (request, response) => {
 
   // Handles the end of the stream.
   request.on('end', () => {
+    console.dir('OnEnd');
+
     // uses Buffer to concatinate the body and convert it to a string
     const bodyString = Buffer.concat(body).toString();
 
     // parses the body string
     const parsedBody = query.parse(bodyString);
 
-    return parsedBody;
+    addUserFromBody(request, response, parsedBody);
   });
 };
 
@@ -79,41 +121,9 @@ const getUsersHead = (request, response) => {
 
 
 const addUser = (request, response) => {
-  const accept = request.headers.accept.split(',');
-  const jsonResponse = {
-    message: 'Both name and age are required in the response.',
-  };
+  console.log('Running addUser()');
 
-  const body = parseBody(request, response);
-  if (!body) {
-    return response;
-  }
-
-  console.dir(body);
-
-  if (!body.name || !body.age) {
-    jsonResponse.id = 'missingParams';
-    if (accept[0] === 'text/xml') {
-      const xmlObj = jsonMessageToXML(jsonResponse);
-      baseResponse.writeResponse(response, 400, xmlObj, accept[0]);
-    } else {
-      baseResponse.writeResponse(response, 400, JSON.stringify(jsonResponse), 'application/json');
-    }
-  }
-
-  const userObj = {
-    name: body.name,
-    age: body.age,
-  };
-
-  const newUser = !(users[body.name]);
-  users[body.name] = userObj;
-
-  if (!newUser) {
-    baseResponse.writeErrorHead(response, 204, accept);
-  } else {
-    baseResponse.writeError(response, 201, accept);
-  }
+  parseBody(request, response);
 };
 
 
